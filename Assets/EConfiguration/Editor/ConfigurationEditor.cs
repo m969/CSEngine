9cc1018d5c70e22e81957a201d2ed3371ed6b91a
yangchengming <yangchengming@jtlxz.com>
2020-10-15 21:04:18 +0800

    up

---
+++ Assets/EConfiguration/Editor/ConfigurationEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.OdinInspector;
+
+namespace EConfiguration
+{
+    public class ConfigurationEditor : OdinEditorWindow
+    {
+        [InlineEditor]
+        public ConfigurationDatabase ConfigurationDatabase;
+
+
+        [MenuItem("Window/ConfigurationEditor")]
+        private static void OpenWindow()
+        {
+            var window = GetWindow<ConfigurationEditor>();
+            window.Show();
+        }
+
+        protected override void OnGUI()
+        {
+            base.OnGUI();
+
+        }
+    }
+}
---
+++ Assets/EConfiguration/Runtime/ConfigurationDatabase.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace EConfiguration
+{
+    public enum TableType { Object, Int, Float, Text }
+
+    [CreateAssetMenu(fileName = "ConfigurationDatabase", menuName = "EConfiguration/ConfigurationDatabase")]
+    public class ConfigurationDatabase : SerializedScriptableObject
+    {
+        [EnumToggleButtons]
+        [HideLabel]
+        public TableType TableType;
+        [ShowIf("TableType", TableType.Object)]
+        public Dictionary<string, Object> ObjectConfigs;
+        [ShowIf("TableType", TableType.Int)]
+        public Dictionary<string, int> IntConfigs;
+        [ShowIf("TableType", TableType.Float)]
+        public Dictionary<string, float> FloatConfigs;
+        [ShowIf("TableType", TableType.Text)]
+        public Dictionary<string, string> TextConfigs;
+
+        public T GetAsset<T>(string key) where T : Object
+        {
+            if (!ObjectConfigs.ContainsKey(key))
+                return null;
+            return ObjectConfigs[key] as T;
+        }
+
+        public GameObject GetGameObject(string key)
+        {
+            return GetAsset<GameObject>(key);
+        }
+
+        public int GetInt(string key)
+        {
+            if (!IntConfigs.ContainsKey(key))
+                return 0;
+            return IntConfigs[key];
+        }
+
+        public float GetFloat(string key)
+        {
+            if (!FloatConfigs.ContainsKey(key))
+                return 0f;
+            return FloatConfigs[key];
+        }
+
+        public string GetText(string key)
+        {
+            if (!TextConfigs.ContainsKey(key))
+                return "";
+            return TextConfigs[key];
+        }
+    }
+}
